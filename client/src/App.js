698871f9b5856fc2fa71d44177ad600242769ffa
Smit Rajput <smitrajputtd@gmail.com>
2019-01-28 04:11:29 +0530

    Add home page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import { Button, Menu } from 'semantic-ui-react'
 import SimpleStorageContract from "./contracts/SimpleStorage.json";
 import getWeb3 from "./utils/getWeb3";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { storageValue: 0, web3: null, accounts: null, contract: null, activeItem: '' };
 
   componentDidMount = async () => {
     try {
@@ -45,26 +46,77 @@ class App extends Component {
     const response = await contract.methods.get().call();
 
     // Update state with the result.
-    this.setState({ storageValue: response });
+    this.setState({ storageValue: response, account: accounts[0] });
   };
+
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name });
+
+  // handleClick(event) {
+  //   const contract = this.state.contract;
+  //   const account = this.state.account;
+
+  //   contract.methods.set.send(3, {from: account})
+  //   .then(result => {
+  //     return contract.get.call()
+  //   }).then(result => {
+  //     return this.setState({storageValue: result.c[0]})
+  //   })
+  // }
+
+  // <Button onClick={this.handleClick.bind(this)}>Set Storage</Button>
 
   render() {
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
+
+    const { activeItem } = this.state;
+
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 40</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <h1>BountyDapp</h1>
+        <div>
+          <Button primary>Become a Bounty-Creator</Button>
+
+        </div>
+
+        
+          <Menu fluid widths={5}>
+            <Menu.Item primary name='Bounty Creators' />
+            <Menu.Item name='Address' />
+            <Menu.Item name='Number of Bounties created' />
+            <Menu.Item name='Total Funding' />
+            <Menu.Item name='Solve' />
+          </Menu>
+          <Menu fluid widths={5}>
+            <Menu.Item name='Adam' active={activeItem === 'buy'} onClick={this.handleItemClick} />
+            <Menu.Item name='0x03j24u23i4h887HUUHH8h4r35trewerw3' />
+            <Menu.Item name='45' />
+            <Menu.Item name='4 ETH' />
+            <Menu.Item>
+              <Button primary>Hunt</Button>
+            </Menu.Item>
+          </Menu>
+
+          <Menu fluid widths={5}>
+            <Menu.Item name='Ramesh' active={activeItem === 'buy'} onClick={this.handleItemClick} />
+            <Menu.Item name='0x03j24u23i4h887HUUHH8h4r35trewerw3' />
+            <Menu.Item name='34' />
+            <Menu.Item name='4 ETH' />
+            <Menu.Item>
+              <Button primary>Hunt</Button>
+            </Menu.Item>
+          </Menu>
+
+          <Menu fluid widths={5}>
+            <Menu.Item name='Bryan' active={activeItem === 'buy'} onClick={this.handleItemClick} />
+            <Menu.Item name='0x03j24u23i4h887HUUHH8h4r35trewerw3' />
+            <Menu.Item name='11' />
+            <Menu.Item name='4 ETH' />
+            <Menu.Item>
+              <Button primary>Hunt</Button>
+            </Menu.Item>
+          </Menu>
       </div>
     );
   }
